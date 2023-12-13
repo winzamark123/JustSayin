28025afd55d7624d4a2448d1e847b03a034a01b6
Win Cheng <winzamark12@gmail.com>
2023-12-13 11:33:10 -0800

    Frontend; Homepage Structure

--- frontend/pages/HomePage.js
+++ frontend/pages/HomePage.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { Dimensions, StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import { useEffect, useState } from "react";
 import { fetchUserFromBackend } from '../api/userAPI';
+import tempUserIMG from '../assets/tempUser.png';
+import { colorPalette } from "../components/theme";
 // import { NavBar } from "../components/navBar";
 
 export default function Home(props) {
@@ -26,24 +28,83 @@ export default function Home(props) {
 
 
     return (
-        <View style={homeStyles.background}>
-            <View style={homeStyles.container}>
-                <Text>Home Page - UserID: {userID}</Text>
-                <Text>{JSON.stringify(user, null, 2)}</Text>
-                <Text>{user.username}</Text>
+        <SafeAreaView style={homeStyles.safeArea}>
+            <View style={homeStyles.background}>
+                <View style={homeStyles.container}>
+                    <View style={homeTop.container}>
+                        <Image source={tempUserIMG} style={homeTop.userProfileImage}></Image>
+                        <View style={homeTop.text}>
+                            <Text>Have a nice day</Text>
+                            <Text>{user.username}</Text>
+                        </View>
+                    </View>
+                    <View style={homeMain.container}>
+                        <Text style={homeMain.text}>Quote of the Day</Text>
+                        <View style={homeMain.quoteBox}>
+
+                        </View>
+                    </View>
+                    <Text>Home Page - UserID: {userID}</Text>
+                    <View style={savedQuotes.container}></View>
+                    {/* <Text>{JSON.stringify(user, null, 2)}</Text> */}
+                    {/* <Text>{user.username}</Text> */}
+                </View>
+                {/* <NavBar /> */}
             </View>
-            {/* <NavBar /> */}
-        </View>
+        </SafeAreaView>
     )
 }
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
+const savedQuotes = StyleSheet.create({
+    container: {
+        borderWidth: 1,
+        borderColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+const homeMain = StyleSheet.create({
+    container: {
+        borderWidth: 1,
+        borderColor: colorPalette.mainColor,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    quoteBox: {
+        height: 233,
+        width: 370,
+        backgroundColor: 'red',
+        borderRadius: 11,
+
+    },
+    text: {
+        alignSelf: 'flex-start',
+    },
+});
+const homeTop = StyleSheet.create({
+    text: {
+        flexDirection: 'column',
+    },
+    container: {
+        flexDirection: 'row',
+        backgroundColor: '#F0F0D1',
+        width: windowWidth,
+
+        borderWidth: 1,
+        borderColor: 'black',
+    },
+    userProfileImage: {
+        width: 50,
+        height: 50,
+    }
+});
 const homeStyles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        // justifyContent: "center",
+        // alignItems: "center",
         width: windowWidth,
         height: windowHeight
     },
@@ -54,6 +115,10 @@ const homeStyles = StyleSheet.create({
         // justifyContent: 'center',
         width: windowWidth,
         height: windowHeight
+    },
+    safeArea: {
+        flex: 1,
+        backgroundColor: '#F0F0D1',
     }
 
 })
