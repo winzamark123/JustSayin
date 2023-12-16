08d94079c0b701d36173da9b2e97e0b3eebc5ccb
Win Cheng <winzamark12@gmail.com>
2023-12-16 14:39:01 -0800

    Tests; UserRoutes and SayingRoutes Tests complete

--- backend/tests/sayingRoutes.test.js
+++ backend/tests/sayingRoutes.test.js
@@ -1,32 +1,60 @@
 const request = require('supertest');
 const express = require('express');
-const sayingController = require('../controllers/sayingController');
-const app = express();
-require('dotenv').config({ path: "../.env.local" });
-
 const mongoose = require('mongoose');
+const sayingController = require('../controllers/sayingController');
+const sayingModel = require('../models/sayingModel');
 
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const app = express();
+let mongoServer;
 
 app.use(express.json());
 app.get('/api/sayings/random', sayingController.getRandomSaying);
 app.get('/api/sayings/', sayingController.getAllSayings);
 
 
+const mockSayings = [
+    {
+        quote: "Test Quote 1",
+        author: "Test Author 1",
+        category: "Test Category 1"
+    },
+    {
+        quote: "Test Quote 2",
+        author: "Test Author 2",
+        category: "Test Category 2"
+    },
+    {
+        quote: "Test Quote 3",
+        author: "Test Author 3",
+        category: "Test Category 3"
+    }
+]
+
+const insertMockData = async () => {
+    for (const saying of mockSayings) {
+        const newSaying = new sayingModel(saying);
+        await newSaying.save();
+    }
+    // Repeat for other collections/models as necessary
+}
+
 //Connect to MONGODB
 beforeAll(async () => {
     // Connect to MongoDB Atlas
-    await mongoose.connect(process.env.MONGO_URI, {
+    mongoServer = await MongoMemoryServer.create();
+    const mongoUri = mongoServer.getUri();
+
+    await mongoose.connect(mongoUri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         serverSelectionTimeoutMS: 20000 // Optional: Increase timeout
     });
+
+    await insertMockData();
+
 });
-
-afterAll(async () => {
-    await mongoose.disconnect();
-});
-
-
 
 describe('Get Saying Routes', () => {
     test('should get a random saying', async () => {
@@ -49,10 +77,16 @@ describe('Get Saying Routes', () => {
             .expect('Content-Type', /json/)
             .expect(200);
 
+        console.log(res.body);
         expect(Array.isArray(res.body)).toBeTruthy();
         expect(res.body[0]).toHaveProperty('_id');
         expect(res.body[0]).toHaveProperty('quote');
         expect(res.body[0]).toHaveProperty('author');
         expect(res.body[0]).toHaveProperty('category');
     }, 10000);
+});
+
+afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
 });
--- backend/tests/userRoutes.test.js
+++ backend/tests/userRoutes.test.js
@@ -3,7 +3,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userController = require('../controllers/userController');
 const userModel = require('../models/userModel');
-const userRoutes = require('../routes/userRoutes');
 
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
@@ -12,16 +11,23 @@ require('dotenv').config({ path: "../.env.local" });
 
 const app = express();
 let mongoServer;
+let userID;
+let testUid = "defaultUid";
 
 
 app.use(express.json());
 
 const mockAuthMiddleware = (req, res, next) => {
-    req.uid = "firebaseUser1";
+    req.uid = testUid;
     next();
 }
 
-app.use('/api/users', mockAuthMiddleware, userRoutes);
+const setTestUid = (uid) => {
+    testUid = uid;
+}
+
+app.post('/api/users/', mockAuthMiddleware, userController.createUser);
+app.get('/api/users/:userID', mockAuthMiddleware, userController.getUser);
 
 const mockUsers = [
     {
@@ -63,19 +69,23 @@ beforeAll(async () => {
 
     //inserting MockData
     await insertMockData();
+
 });
-
-afterAll(async () => {
-    await mongoose.disconnect();
-    await mongoServer.stop();
-});
-
-
 
 describe('Get User Routes', () => {
     test('should get user details', async () => {
+        setTestUid('firebaseUser1');
+        
+        const createdUser = await userModel.findOne({ firebaseID: 'firebaseUser1' });
+
+        if (!createdUser) {
+            throw new Error('User not found');
+        };
+        userID = createdUser._id.toString();
+
         const res = await request(app)
             .get(`/api/users/${userID}`)
+            .set('Authorization', `Bearer mock-token`)
             .set('Accept', 'application/json');
 
         if (res.statusCode !== 200) {
@@ -86,7 +96,7 @@ describe('Get User Routes', () => {
         expect(res.headers['content-type']).toMatch(/json/);
 
         expect(res.body).toHaveProperty('_id');
-        expect(res.body).toHaveProperty('firebaseID', userID);
+        expect(res.body).toHaveProperty('firebaseID', 'firebaseUser1');
         expect(res.body).toHaveProperty('username');
         expect(res.body.savedCategories).toBeInstanceOf(Array);
     }, 10000);
@@ -95,14 +105,20 @@ describe('Get User Routes', () => {
 // Test cases
 describe('Post User Routes', () => {
     test('should create a new user', async () => {
+        setTestUid('firebaseUser3');
+
         const res = await request(app)
             .post('/api/users/')
+            .set('Authorization', `Bearer mock-token`)
             .send({
                 firebaseID: 'firebaseUser3',
                 username: 'TestUser3',
-                savedSayings: [],
-                savedCategories: [new mongoose.Types.ObjectId(), new mongoose.Types.ObjectId(), new mongoose.Types.ObjectId()],
             });
         expect(res.statusCode).toEqual(201);
     });
+});
+
+afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
 });
