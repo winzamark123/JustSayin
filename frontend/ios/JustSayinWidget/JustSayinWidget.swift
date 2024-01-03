cb2f9fe7e765e3d7414e66453d8a6a7a4576df94
Win Cheng <winzamark12@gmail.com>
2024-01-03 14:31:40 +0800

    ios; Widget Styling

--- frontend/ios/JustSayinWidget/JustSayinWidget.swift
+++ frontend/ios/JustSayinWidget/JustSayinWidget.swift
@@ -57,12 +57,12 @@ struct JustSayinWidgetEntryView : View {
     var body: some View {
       ZStack{
         ContainerRelativeShape()
-          .fill(.white.gradient)
+          .fill(Color(hex: "#262626"))
         VStack{
-          Text(entry.data.quote).font(.custom("Poppins-Bold", size: 15)).padding()
+          Text(entry.data.quote).font(.custom("Poppins-Bold", size: 15)).padding().foregroundColor(Color(hex: "#FFFFFF")).lineLimit(5).minimumScaleFactor(0.5)
           HStack{
             Spacer()
-            Text(entry.data.author).font(.custom("Poppins-SemiBold", size: 11)).padding().multilineTextAlignment(.trailing)
+            Text(entry.data.author).font(.custom("Poppins-SemiBold", size: 11)).padding().multilineTextAlignment(.trailing).foregroundColor(Color(hex: "#FFFFFF")).lineLimit(1).minimumScaleFactor(0.5)
           }
         }.padding(.top, 20)
       }
@@ -84,8 +84,34 @@ struct JustSayinWidget: Widget {
 
 struct JustSayinWidget_Previews: PreviewProvider {
     static var previews: some View {
-      let valuesData = ValuesData(quote: "This is a mock quote!", author:"Win Cheng")
+      let valuesData = ValuesData(quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco cillum dolore eu fugiat nulla pariatur", author:"Win Cheng")
       JustSayinWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent(), data: valuesData))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
     }
 }
