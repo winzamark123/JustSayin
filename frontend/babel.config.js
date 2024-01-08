d379a15166b16c9d44c1034a0a9d0220bc1a77e0
Win Cheng <winzamark12@gmail.com>
2024-01-08 08:43:16 -0800

    Debugged NPM and Babel configurations; This current version works without profile pic

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,6 +2,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [['module:react-native-dotenv']],
+    plugins: [
+      ['module:react-native-dotenv', {
+        moduleName: '@env',
+        path: '../.env.local', // specify your custom env file name here
+        blocklist: null,
+        allowlist: null,
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      }],
+    ],
   };
 };
--- frontend/firebaseConfig.js
+++ frontend/firebaseConfig.js
@@ -3,6 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID } from '@env';
 
+console.log("FIREBASE_API_KEY:", FIREBASE_API_KEY);
+
 const firebaseConfig = {
     apiKey: FIREBASE_API_KEY,
     authDomain: FIREBASE_AUTH_DOMAIN,
