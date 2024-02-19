d80a426e0101dedb7f3ef14bd3c654dbf6bb9177
Win Cheng <winzamark12@gmail.com>
2024-02-18 17:58:26 -0800

    OK Notification Works Directly through server

--- backend/controllers/notificationController.js
+++ backend/controllers/notificationController.js
@@ -1,30 +1,6 @@
 const admin = require('firebase-admin');
 
-exports.sendTestNotification = async (req, res) => {
-    console.log("Sending Test Notification")
-    const { deviceToken } = req.body;
-    console.log("Device Token: ", deviceToken);
-
-    const message = {
-        notification: {
-            title: 'Test Notification',
-            body: 'This is a test notification!'
-        },
-        token: deviceToken
-    };
-
-    try {
-        const response = await admin.messaging().send(message);
-        console.log('Successfully sent message:', response);
-        res.status(200).json({ message: "Test Notification Sent" });
-    } catch (error) {
-        console.log('Error sending message:', error);
-        res.status(500).json({ message: "Error sending test notification" });
-    }
-
-}
-
-exports.sendNotification = async () => {
+exports.sendNotificationToAllUsers = async () => {
     console.log("Sending Notification")
     const message = {
         notification: {
--- backend/server.js
+++ backend/server.js
@@ -47,8 +47,8 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
         // cron schedule ever minute
         cron.schedule('* * * * *', async () => {
-            console.log("Running the notification task")
-            notificationController.sendNotification();
+            // console.log("Running the notification task")
+            // notificationController.sendNotification();
         });
 
 
