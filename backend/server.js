e35e9152062e85463bcad2ff4bd273e67e31e62e
Win Cheng <winzamark12@gmail.com>
2024-02-19 11:44:00 -0800

    Disabled the Test

--- backend/server.js
+++ backend/server.js
@@ -45,10 +45,10 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
         console.log("Running the daily saying generation task");
 
         // cron schedule ever minute
-        cron.schedule('* * * * *', async () => {
-            console.log("Running the notification task")
-            notificationController.sendNotificationTest();
-        });
+        // cron.schedule('* * * * *', async () => {
+        //     console.log("Running the notification task")
+        //     notificationController.sendNotificationTest();
+        // });
 
 
         cron.schedule('0 0 * * *', async () => {
