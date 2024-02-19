49b21207885fdd5cbe37626ecc3bbb4c17d4fe3f
Win Cheng <winzamark12@gmail.com>
2024-02-19 11:25:24 -0800

    Before Creating UNNotification Service Extension

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,17 @@ import { AppRegistry } from 'react-native';
 import messaging from '@react-native-firebase/messaging';
 import App from './App'; // Make sure the path to your App component is correct
 
+const toggleWidget = (dailySaying, author) => {
+    const {RNSharedWidget} = NativeModules;
+
+    RNSharedWidget.setData('justSayinWidgetKey', JSON.stringify({
+        quote: dailySaying.sayingID.quote ?? "Unknown",
+        author: dailySaying.sayingID.author ?? "Author",
+    }), (status) => {
+        console.log('Widget status: ', status);
+    });
+};
+
 // Register background handler
 messaging().setBackgroundMessageHandler(async remoteMessage => {
     console.log('Message handled in the background!', remoteMessage);
