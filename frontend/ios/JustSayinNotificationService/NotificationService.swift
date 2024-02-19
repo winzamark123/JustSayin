d3cac029b4c966c3b225f833eedd5cc4235c3a41
Win Cheng <winzamark12@gmail.com>
2024-02-19 11:55:05 -0800

    Logging on Notification Service

--- frontend/ios/JustSayinNotificationService/NotificationService.swift
+++ frontend/ios/JustSayinNotificationService/NotificationService.swift
@@ -1,28 +1,39 @@
-//
-//  NotificationService.swift
-//  JustSayinNotificationService
-//
-//  Created by Win Cheng on 19/2/2567 BE.
-//
-
 import WidgetKit
 import UserNotifications
 
 class NotificationService: UNNotificationServiceExtension {
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        // Log the receipt of the notification
+        print("NotificationService: didReceive called")
+
         // Process the notification content and extract the data
         let userInfo = request.content.userInfo
+        print("NotificationService: UserInfo received - \(userInfo)")
+
         if let dailySaying = userInfo["dailySaying"] as? String,
            let author = userInfo["author"] as? String {
             // Update the shared user defaults (used by the widget)
-            let sharedDefaults = UserDefaults(suiteName: "group.frontend")
-            sharedDefaults?.setValue(dailySaying, forKey: "quote")
-            sharedDefaults?.setValue(author, forKey: "author")
-            sharedDefaults?.synchronize()
+            if let sharedDefaults = UserDefaults(suiteName: "group.frontend") {
+                print("NotificationService: Updating shared user defaults")
+                sharedDefaults.setValue(dailySaying, forKey: "quote")
+                sharedDefaults.setValue(author, forKey: "author")
+                let didSynchronize = sharedDefaults.synchronize()
 
-            // Tell the system to update the widget
-            WidgetCenter.shared.reloadAllTimelines()
+                if didSynchronize {
+                    print("NotificationService: Successfully updated user defaults")
+                } else {
+                    print("NotificationService: Failed to synchronize user defaults")
+                }
+
+                // Tell the system to update the widget
+                print("NotificationService: Requesting widget timeline reload")
+                WidgetCenter.shared.reloadAllTimelines()
+            } else {
+                print("NotificationService: Unable to access shared user defaults")
+            }
+        } else {
+            print("NotificationService: Failed to extract dailySaying or author from userInfo")
         }
 
         // Call the content handler
